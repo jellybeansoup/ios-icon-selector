43dbd10e35915df52e36adc450547976292af249
Daniel Farrelly <daniel@jellystyle.com>
2021-01-22 10:35:00 +1100

    Update the copyright year

--- src/IconSelector/IconSelector.h
+++ src/IconSelector/IconSelector.h
@@ -1,5 +1,5 @@
 //
-// Copyright © 2019 Daniel Farrelly & Curtis Herbert
+// Copyright © 2021 Daniel Farrelly & Curtis Herbert
 //
 // Redistribution and use in source and binary forms, with or without modification,
 // are permitted provided that the following conditions are met:
